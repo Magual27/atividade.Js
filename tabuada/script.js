472111f47adc646f3eb450b40ccd8405f3155b29
Magual27 <miguenrique27@gmail.com>
2023-05-03 09:57:42 -0300

    atividade tabuada

---
+++ tabuada/script.js
@@ -0,0 +1,12 @@
+function tabuada(){
+    let trava = 0
+    let numero = Number(document.getElementById('numero').value)
+    let resultado = document.getElementById('resultado')
+
+    resultado.innerHTML = ''
+    while (trava <= 9) {
+        trava++
+        let Numero = numero * trava 
+        resultado.innerHTML += `<div>${numero} x ${trava} = ${Numero}</div>`
+    }
+}       
