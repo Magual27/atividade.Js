23897752faedaf4d29fdc833c6e9d875d0183ead
Magual27 <miguenrique27@gmail.com>
2023-05-04 10:55:02 -0300

    estilizando a página HTML

--- atividade saque/script.js
+++ atividade saque/script.js
@@ -2,35 +2,45 @@ let Saldo = document.getElementById('saldo')
 let extrato = document.getElementById('extrato')
 let saldo = 700
 Saldo.innerHTML = `
-    <p>Saldo Atual: <strong>${saldo}</strong></p>
+    <p>Saldo Atual: <strong>R$${saldo}</strong></p>
 `
 function Sacar(){
     let valorSacar = Number(document.getElementById('id_saque').value)
-    if (saldo > 0 && valorSacar <= saldo){ 
+    if (valorSacar == "") {
+        alert("ERRO: Valor mínimo para saque -> R$0,01")
+    } else if (valorSacar == 0) {
+        alert("ERRO: Insira algum valor para sacar")
+    } else if (saldo > 0 && valorSacar <= saldo){ 
         saldo -= valorSacar
         Saldo.innerHTML = `
             <p>Saldo Atual: <strong>${saldo}</strong></p>
         `
         extrato.innerHTML += `
             <p id="sacando">
-                -$${valorSacar}
+                -R$${valorSacar}
             </p>
         `
     } else if (valorSacar > saldo && saldo > 0) {
         alert(`ERRO: Valor de saque ultrapassa o saldo, somente sacar ate ${saldo}`)
     } else {
         alert("ERRO: Saldo insuficiente para sacar")
-    }
+    } 
 } 
 function Depositar(){
     let valorDepositar = Number(document.getElementById('id_deposito').value)
-    saldo += valorDepositar
-    Saldo.innerHTML = `
-        <p>Saldo Atual: <strong>${saldo}</strong></p>
-    `
-    extrato.innerHTML += `
-        <p id="depositando">
-            +$${valorDepositar}
-        </p>
-    `
+    if (valorDepositar == 0) {
+        alert("ERRO: Valor minimo para doposito -> R$0,01")
+    } else if (valorDepositar == "") {
+        alert("ERRO: Insira algum valor para depositar")
+    } else {
+        saldo += valorDepositar
+        Saldo.innerHTML = `
+            <p>Saldo Atual: <strong>R$${saldo}</strong></p>
+        `
+        extrato.innerHTML += `
+            <p id="depositando">
+                +R$${valorDepositar}
+            </p>
+        `
+    }
 }
