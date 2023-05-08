57ca36275a270708dd4ba51d0faea2438837bd09
Maguel27 <miguenrique27@gmail.com>
2023-05-08 15:23:31 -0300

    pequena alteração

--- atividade for/script.js
+++ atividade for/script.js
@@ -1,8 +1,7 @@
 function ordenar(){
-    let n = [1,0,6,3,5,4,6,8,9,2,10]
-    n.sort(compare)
+    let n = [1,0,7,3,5,4,6,8,9,2,10]
+    n.sort(function(a,b){
+        return a - b
+    })
     alert(n)
-}
-function compare(a,b){
-    return a-b
 }
