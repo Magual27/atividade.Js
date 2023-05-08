a5e9a0ada559348a37c301929304cc22e883c672
Maguel27 <miguenrique27@gmail.com>
2023-05-08 15:10:37 -0300

    atualizando atividade escola

--- atividade escola/script.js
+++ atividade escola/script.js
@@ -1,6 +1,7 @@
 function selecionar(){
     let salario =  0.0
     let valorProfessor = 0.0
+    let resultado = document.getElementById('res')
     let professor = document.getElementById('prof').value
     let classe = document.getElementById('sala').value
     let aulas = Number(document.getElementById('qntd_aulas').value)
@@ -36,5 +37,18 @@ function selecionar(){
         default:
             break;
     }     
-    alert(`Professor(a): ${professor}\nClasse: ${classe}\nQuantidade de aulas: ${aulas}\nPreço por aula: R$${valorProfessor}\nTotal: R$${salario.toFixed(2)}`)  
+    console.log(professor)
+    if (professor == 0 && classe == 0 && aulas == 0) {
+        alert('ERRO: Preencha todas opções')
+    } else {
+        resultado.innerHTML = `
+            <p>
+                <strong>
+                    Professor(a) ${professor} foi designado para a sala ${classe} para ${aulas} aula(s)<br>
+                    Preço por aula: R$${valorProfessor}<br>
+                    Total: R$${salario.toFixed(2)}
+                </strong>
+            </p>
+        `
+    }
 }
