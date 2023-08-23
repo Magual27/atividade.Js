4092493c35d37b99d3873b34f8384b8fdc77552b
Magual27 <miguenrique27@gmail.com>
2023-08-23 12:18:23 -0300

    status: incompleto

---
+++ atividade_Node8/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const port = 3000;
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    const auth = true;
+
+    res.render("home", auth);
+});
+
+app.get("/gatos", (req, res) => {
+    const gatos = {
+        nome: "Julia",
+        idade: 2,
+        dono: "Jonathan",
+    };
+
+    res.render("gatos");
+});
+
+app.listen(port, () => {
+    console.log(`Rondando na porta ${port}`);
+});
