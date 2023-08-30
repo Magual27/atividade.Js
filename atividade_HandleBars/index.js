d4cbd55fb412442721c8d10b1d3ec77d7fb329de
Magual27 <miguenrique27@gmail.com>
2023-08-30 09:55:53 -0300

    continua na proxima aula

---
+++ atividade_HandleBars/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const port = 3000;
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("public"));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const auth = true;
+
+  res.render("home", { auth });
+});
+
+app.get("/musicas", (req, res) => {
+  var musicas = [
+    {
+      id: 1,
+      nome: "Jumpman",
+      artista: "Drake",
+    },
+    {
+      id: 2,
+      nome: "Murder on my mind",
+      artista: "YNW Melly",
+    },
+    {
+      id: 3,
+      nome: "Alredy Rich",
+      artista: "Yeat",
+    },
+  ];
+
+  res.render("musicas", { musicas });
+});
+
+app.get("/musica", (req, res) => {
+    res.render(musica)
+});
+
+app.get("/musicas/cadastrar", (req, res) => {
+  res.render("cadastrarMusica");
+});
+
+app.post("/musicas/enviar", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Rondando na porta ${port}`);
+});
