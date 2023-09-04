b933dc199e221a751a26d0d2bb02f7d60d878c24
Magual27 <miguenrique27@gmail.com>
2023-09-04 19:12:57 -0300

    atividade incompleta

--- atividade_mysql/utilizando_partials/index.js
+++ atividade_mysql/utilizando_partials/index.js
@@ -8,32 +8,31 @@ const musicas = require("./musicas"); // importando modulo interno musicas
 
 //BODY
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 // importar JSON
 app.use(express.json());
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "bancodedados",
-  database: "minhaEmpresa_db",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "minhaEmpresa_db",
 });
 
 con.connect((err) => {
-  if (err) {
-    console.log("Não funcionou");
-    console.log(err);
-  } else {
-    console.log("Conectado ao Banco de dados");
-  }
+    if (err) {
+        console.log("Não funcionou");
+        console.log(err);
+    } else {
+        console.log("Conectado ao Banco de dados");
+    }
 });
 
-
 const hbs = exphbs.create({
-  partialsDir: ["views/partials"], // --> Uso do Partials
+    partialsDir: ["views/partials"], // --> Uso do Partials
 });
 
 // construção das handlebars
@@ -44,68 +43,65 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 // utilizar rotas/caminhos do modulo musicas
-app.use("/musicas", musicas);
+app.use(musicas);
 
 // vetor de musicas
-const songs = [
-  {
-    id: 1,
-    nome: "When You're Gone",
-    artista: "Avril Lavigne",
-    tipo: "Rock",
-  },
-  {
-    id: 2,
-    nome: "Demons",
-    artista: "Imagine Dragons",
-    tipo: "Rock",
-  },
-  {
-    id: 3,
-    nome: "is This Love",
-    artista: "Bob Marley",
-    tipo: "Reggae",
-  },
-  {
-    id: 4,
-    nome: "Runaway",
-    artista: "AURORA",
-    tipo: "Indie",
-  },
-  {
-    id: 5,
-    nome: "Master of Puppets",
-    artista: "Metalica",
-    tipo: "Rock",
-  },
-  {
-    id: 6,
-    nome: "How you like that",
-    artista: "BLACKPINK",
-    tipo: "Kpop",
-  },
-  {
-    id: 7,
-    nome: "Crawling",
-    artista: "Linkin Park",
-    tipo: "Rock",
-  },
-];
+// const songs = [
+//   {
+//     id: 1,
+//     nome: "When You're Gone",
+//     artista: "Avril Lavigne",
+//     tipo: "Rock",
+//   },
+//   {
+//     id: 2,
+//     nome: "Demons",
+//     artista: "Imagine Dragons",
+//     tipo: "Rock",
+//   },
+//   {
+//     id: 3,
+//     nome: "is This Love",
+//     artista: "Bob Marley",
+//     tipo: "Reggae",
+//   },
+//   {
+//     id: 4,
+//     nome: "Runaway",
+//     artista: "AURORA",
+//     tipo: "Indie",
+//   },
+//   {
+//     id: 5,
+//     nome: "Master of Puppets",
+//     artista: "Metalica",
+//     tipo: "Rock",
+//   },
+//   {
+//     id: 6,
+//     nome: "How you like that",
+//     artista: "BLACKPINK",
+//     tipo: "Kpop",
+//   },
+//   {
+//     id: 7,
+//     nome: "Crawling",
+//     artista: "Linkin Park",
+//     tipo: "Rock",
+//   },
+// ];
 
 app.get("/", (req, res) => {
-  res.render("home", { songs });
-});
-
-app.get("/musicas/:id", (req, res) => {
-  const musicaSelecionada = songs[parseInt(req.params.id) - 1];
-
-  res.render("musica", { choicedMusic: musicaSelecionada });
+    con.execute("SELECT * FROM musicas", (err, query) => {
+       res.render("home", { query });
+    })
+   
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+    res.status(404).render("404");
 });
 
 app.listen(port, () => {
-  console.log("Projeto funcionando, acesse localhost:" + port);
+    console.log("Projeto funcionando, acesse localhost:" + port);
 });
--- atividade_mysql/utilizando_partials/musicas/index.js
+++ atividade_mysql/utilizando_partials/musicas/index.js
@@ -1,29 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const exphbs = require('express-handlebars')
+const exphbs = require("express-handlebars");
 const app = express();
+const mysql = require("mysql2");
 
-
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "minhaEmpresa_db",
+});
 
 const hbs = exphbs.create({
-    partialsDir: ['views/partials'] // --> Uso do Partials 
-})
+    partialsDir: ["views/partials"], // --> Uso do Partials
+});
 
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // ----------- endpoints --------------
-router.get("/cadastrar", (req, res) =>{
-    res.render("cadastrarMusica")
-})
 
+app.get("/musicas/:id", (req, res) => {
+    const musicaSelecionada = songs[parseInt(req.params.id) - 1];
 
-router.post("/salvar", (req, res) =>{
+    res.render("musica", { choicedMusic: musicaSelecionada });
+});
 
-    console.log(req.body)
+router.get("/musicas/cadastrar", (req, res) => {
+    res.render("cadastrarMusica");
+});
 
-})
-
-
+router.post("/musicas/salvar", (req, res) => {
+    con.execute(
+        `
+        INSERT INTO musicas ("nome", "artista", "categoria")
+        VALUES ("${req.body.nome}", "${req.body.artista}", "${req.body.tipo}")
+    `,
+        (err, query) => {
+            res.render("cadastrarMusica", { query });
+        }
+    );
+    // console.log(req.body)
+});
 
 module.exports = router;
